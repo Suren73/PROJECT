41dd41e8b99a661f4254949b2b5c888683a79190
Andrey Surkov <andronsurkov73@gmail.com>
2025-03-18 13:08:14 +0300

    edit App without jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,28 +1,39 @@
 //* Декларативный стиль
+import { createElement } from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
 
 //* Императивный стиль
 export const App = () => {
-	const Year = new Date().getFullYear();
+	const year = new Date().getFullYear();
 	return (
 		//* Декларативный стиль
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{Year}</div>
-			</header>
-		</div>
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+				createElement(
+					'p',
+					null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('div', null, year),
+			),
+		)
 	);
 };
